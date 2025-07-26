1083928d9a470b0772b0005e2eac5c4ca410cbc3
Shashank Mishra <mishrashashank2106@gmail.com>
2025-07-26 20:14:38 +0530

    deployment update 4

--- src/api/bugService.ts
+++ src/api/bugService.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
 
 const API = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL, 
 });
 
 API.interceptors.request.use((config) => {
@@ -15,10 +15,9 @@ API.interceptors.request.use((config) => {
   return config;
 });
 
-
 export const fetchBugs = () => API.get('bugs/');
 export const fetchBugById = (id: number) => API.get(`bugs/${id}/`);
 export const createBug = (bugData: any) => API.post('bugs/', bugData);
-export const loginUser = (credentials: any) => axios.post(`${BASE_URL}/login/`, credentials); // Can also use API.post('login/', credentials)
+export const loginUser = (credentials : any) => API.post('login/', credentials);
 export const registerUser = (userData: any) => API.post('register/', userData);
 export const deleteBug = (id: number) => API.delete(`bugs/${id}/`);
