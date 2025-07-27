f28ab62d1df00f147ae487806dc6fcf30fb91364
Shashank Mishra <mishrashashank2106@gmail.com>
2025-07-27 10:51:19 +0530

    deployment update frontend

--- src/api/bugService.ts
+++ src/api/bugService.ts
@@ -10,11 +10,17 @@ API.interceptors.request.use((config) => {
     config.headers.Authorization = `Token ${token}`;
   }
   return config;
-});
+});       
 
 export const fetchBugs = () => API.get('bugs/');
 export const fetchBugById = (id: number) => API.get(`bugs/${id}/`);
 export const createBug = (bugData: any) => API.post('bugs/', bugData);
-export const loginUser = (credentials: any) => API.post('login/', credentials);
+export const loginUser = (credentials: any) =>
+  API.post('login/', credentials, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
 export const registerUser = (userData: any) => API.post('register/', userData);
 export const deleteBug = (id: number) => API.delete(`bugs/${id}/`);
